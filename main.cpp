8e9b8ad8774c1160f0ab5e2489259c7a3c726324
santiagopr123 <santiago.pereira@udea.edu.co>
2020-10-17 16:42:02 -0500

    septimo commit

--- main.cpp
+++ main.cpp
@@ -2,23 +2,37 @@
 #include <math.h>
 
 using namespace std;
-void momento_ofensivo(float, float);
+void momento_ofensivo(double,double, double);
 
 int main()
 {
-    float velocidad_inicial = 0,tiempo = 0, angulo = 0;
+    double velocidad_inicial = 0,time = 0, angulo = 0;
 
-    cout<<"ingrese la altura respectiva: "<<endl;cin>>tiempo;
+    cout<<"ingrese la altura respectiva: "<<endl;cin>>time;
     cout<<"ingrese la velocidad inicial: "<<endl;cin>>velocidad_inicial;
     cout<<"ingrese el angulo de tiro: "<<endl;cin>>angulo;
+
+    momento_ofensivo(time, velocidad_inicial, angulo);
 
 
     return 0;
 }
 
-void momento_ofensivo(float tiempo, float velocidad,float angul)
+void momento_ofensivo(double tiempo, double velocidad,double angul)
 {
-    float ;
+    double velocidad_y,pi = 3.1416,gravitacion = 9.81;
+
+    double y;
+
+    velocidad_y = (velocidad*sin(pi/angul))-gravitacion*tiempo;
+
+    y = (velocidad_y*tiempo)-(1/2)*gravitacion*(tiempo*tiempo);
+
+    cout<<velocidad_y<<endl;
+    cout<<y;
+
+
+
 
 
 
