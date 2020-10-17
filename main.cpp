7bf2fc9717766650b1dc53e8f4bca8a5ce3d266a
santiagopr123 <santiago.pereira@udea.edu.co>
2020-10-17 15:53:10 -0500

    quinto commit(haciendo las funciones)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
 using namespace std;
+void momento_ofensivo(int, int);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout<<"ingrese la altura respectiva: "<<endl;
+    cout<<"ingrese la velocidad inicial: "<<endl;
     return 0;
+}
+
+void momento_ofensivo(float altura, float velocidad);
+{
+    float rango = 0, altura_m = 0,gravitacion = 9,81;
+
+    altura_m = (velocidad*velocidad)/(2*gravitacion);
+
 }
